2d5ff059f570346795e6166c4f51cc331370890d
David <david.lopez.rivera.2003@gmail.com>
2025-09-24 16:09:11 -0500

    some correction

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app.component'; // ✅ Cambiar importación
 
-describe('App', () => {
+describe('AppComponent', () => { // ✅ Cambiar nombre del describe
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [AppComponent], // ✅ Cambiar por AppComponent
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent); // ✅ Cambiar por AppComponent
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent); // ✅ Cambiar por AppComponent
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, yuxbank');
