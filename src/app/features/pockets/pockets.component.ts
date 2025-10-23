e3e925689977f5a9c03a34e09d3ea2245142f685
David <david.lopez.rivera.2003@gmail.com>
2025-10-22 19:20:28 -0500

    add pockets complement

--- src/app/features/pockets/pockets.component.ts
+++ src/app/features/pockets/pockets.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PocketManagerComponent } from '../../shared/pocket-manager/pocket-manager.component';
 
 @Component({
   selector: 'app-pockets',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PocketManagerComponent],
   templateUrl: './pockets.component.html',
-  styleUrl: './pockets.component.css'
+  styleUrls: ['./pockets.component.css']
 })
 export class PocketsComponent {
+  showPocketManager = false;
+
+  openPocketManager() {
+    this.showPocketManager = true;
+  }
+
+  closePocketManager() {
+    this.showPocketManager = false;
+  }
 
 }
